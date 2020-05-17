8a3bac68a4a0e7dedb17ec956b6e6567f2d45a9b
Joalbert <joalbertgonzalez@gmail.com>
2020-05-17 14:23:11 -0500

    [ADD] lives and level texts added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import time
 import random
 
+pg.font.init()
+
 WIDTH, HEIGHT = 750, 750
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Space Shooter')
@@ -28,9 +30,20 @@ def main():
     run = True
     FPS = 60
     clock = pg.time.Clock()
+    level = 0
+    lives = 5
+    main_font = pg.font.SysFont('comicsans', 50)
 
     def redraw_windown():
         WIN.blit(BG, (0, 0))
+
+        # draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
         pg.display.update()
 
     while run:
